bf332b327f8c764064725f01689b686bb1734197
wangliang <eric.wang@everbridge.com>
2016-05-24 11:21:00 +0800

    add

--- salary-calc/ViewCotroller/EWRootViewController.m
+++ salary-calc/ViewCotroller/EWRootViewController.m
@@ -9,16 +9,34 @@
 #import "EWRootViewController.h"
 
 @interface EWRootViewController ()
-
+{
+    UIBarButtonItem* left;
+    UIBarButtonItem* right;
+}
 @end
 
 @implementation EWRootViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.title = @"calc";
+    left = [[UIBarButtonItem alloc] initWithTitle:@"北京" style:UIBarButtonItemStylePlain target:self action:@selector(changeCity:)];
+    right = [[UIBarButtonItem alloc] initWithTitle:@"设置" style:UIBarButtonItemStylePlain target:self action:@selector(changeSetting:)];
+    self.navigationItem.leftBarButtonItem = left;
+    self.navigationItem.rightBarButtonItem = right;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+}
+
+#pragma mark change city
+-(void) changeCity:(id) sender {
+    
+}
+
+#pragma mark change setting
+-(void) changeSetting:(id) sender {
+
 }
 @end
