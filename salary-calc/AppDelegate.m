31f10ce16209cb32504112f4082855028dd6f10b
亮 <liang.wang@wormpex.com>
2018-04-22 20:34:40 +0800

    add

--- salary-calc/AppDelegate.m
+++ salary-calc/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "CityDao.h"
 
 @interface AppDelegate ()
 
@@ -16,6 +17,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [CityDao sharedInstance];
     return true;
 }
 
