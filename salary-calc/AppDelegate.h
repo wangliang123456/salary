4226d7e4a29066510df32e6dae48cd55ef274372
亮 <liang.wang@wormpex.com>
2018-08-11 12:43:51 +0800

    add

--- salary-calc/AppDelegate.h
+++ salary-calc/AppDelegate.h
@@ -8,10 +8,15 @@
 
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+#import "GDTSplashAd.h"
+
+static NSString *kGDTMobSDKAppId = @"1105344611";
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate,GDTSplashAdDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
+@property (strong, nonatomic) GDTSplashAd *splash;
 
 @end
 
--- salary-calc/AppDelegate.m
+++ salary-calc/AppDelegate.m
@@ -31,6 +31,18 @@
     });
     [GADMobileAds configureWithApplicationID:@"ca-app-pub-6212992129754905~9672509276"];
     [FIRApp configure];
+    
+    //开屏广告初始化并展示代码
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        GDTSplashAd *splash = [[GDTSplashAd alloc] initWithAppId:kGDTMobSDKAppId placementId:@"9040714184494018"];
+        splash.delegate = self;
+        UIImage *splashImage = [UIImage imageNamed:@"Launch"];
+        splash.backgroundImage = splashImage;
+        splash.fetchDelay = 3;
+        [splash loadAdAndShowInWindow:self.window];
+        self.splash = splash;
+    }
+    
     return true;
 }
 
@@ -52,6 +64,62 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     
+}
+
+- (void)splashAdApplicationWillEnterBackground:(GDTSplashAd *)splashAd
+{
+    NSLog(@"%s",__FUNCTION__);
+}
+
+-(void)splashAdSuccessPresentScreen:(GDTSplashAd *)splashAd
+{
+    NSLog(@"%s",__FUNCTION__);
+}
+
+-(void)splashAdFailToPresent:(GDTSplashAd *)splashAd withError:(NSError *)error
+{
+    NSLog(@"%s%@",__FUNCTION__,error);
+    self.splash = nil;
+}
+
+- (void)splashAdWillClosed:(GDTSplashAd *)splashAd
+{
+    NSLog(@"%s",__FUNCTION__);
+}
+-(void)splashAdClosed:(GDTSplashAd *)splashAd
+{
+    NSLog(@"%s",__FUNCTION__);
+    self.splash = nil;
+}
+
+- (void)splashAdWillPresentFullScreenModal:(GDTSplashAd *)splashAd
+{
+    NSLog(@"%s",__FUNCTION__);
+}
+
+- (void)splashAdDidPresentFullScreenModal:(GDTSplashAd *)splashAd
+{
+    NSLog(@"%s",__FUNCTION__);
+}
+
+- (void)splashAdExposured:(GDTSplashAd *)splashAd
+{
+    NSLog(@"%s",__FUNCTION__);
+}
+
+- (void)splashAdClicked:(GDTSplashAd *)splashAd
+{
+    NSLog(@"%s",__FUNCTION__);
+}
+
+- (void)splashAdWillDismissFullScreenModal:(GDTSplashAd *)splashAd
+{
+    NSLog(@"%s",__FUNCTION__);
+}
+
+- (void)splashAdDidDismissFullScreenModal:(GDTSplashAd *)splashAd
+{
+    NSLog(@"%s",__FUNCTION__);
 }
 
 @end
